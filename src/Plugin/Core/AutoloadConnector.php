7e9332d8088c45cc8f76730e2afbb54d3b74298f
Denis Mir <dm@communiacs.de>
2018-06-20 11:28:49 +0200

    Cleans up the autoload connector

--- src/Plugin/Core/AutoloadConnector.php
+++ src/Plugin/Core/AutoloadConnector.php
@@ -2,9 +2,12 @@
 namespace Communiacs\Sw\Composer\Plugin\Core;
 
 use Composer\Composer;
+use Composer\Config;
 use Composer\IO\IOInterface;
+use Composer\Json\JsonFile;
 use Composer\Semver\Constraint\EmptyConstraint;
 use Composer\Util\Filesystem;
+use Composer\Util\RemoteFilesystem;
 
 /**
  * Creates a symlink of the central autoload.php file in the vendor directory of the Shopware core package
@@ -30,6 +33,8 @@ class AutoloadConnector
     protected $filesystem;
 
     /**
+     * @param IOInterface $io
+     * @param Composer $composer
      * @param Filesystem $filesystem
      */
     public function __construct(IOInterface $io = null, Composer $composer = null, Filesystem $filesystem = null)
@@ -39,32 +44,24 @@ class AutoloadConnector
         $this->filesystem = $this->filesystem ?: new Filesystem();
     }
 
-    public function linkAutoLoader($event = null)
+    public function linkAutoLoader()
     {
-        if ($this->composer === null) {
-            // Old plugin called this method, let's be graceful
-            $this->composer = $event->getComposer();
-            $this->io = $event->getIO();
-            $this->io->writeError('<warning>Shopware Composer Plugin incomplete update detected.</warning>');
-            $this->io->writeError('<warning>To fully upgrade to the new Shopware Composer Plugin, call "composer update" again.</warning>');
-        }
-
         if ($this->composer->getPackage()->getName() === 'communiacs/shopware-dev') {
-            // Nothing to do communiacs/shopware is root package
+            $this->io->writeError('<info>Skipping SHOPWARE autoload proxy</info>', true, IOInterface::QUIET);
             return;
         }
 
-        $this->io->writeError('<info>Writing SHOPWARE autoload proxy</info>', true, IOInterface::VERBOSE);
+        $this->io->writeError('<info>Writing SHOPWARE autoload proxy</info>', true, IOInterface::QUIET);
 
         $composerConfig = $this->composer->getConfig();
         $localRepository = $this->composer->getRepositoryManager()->getLocalRepository();
         $package = $localRepository->findPackage('communiacs/shopware-dev', new EmptyConstraint());
 
 
-        $defaultVendorDir = \Composer\Config::$defaultConfig['vendor-dir'];
+        $defaultVendorDir = Config::$defaultConfig['vendor-dir'];
 
         $packagePath = $this->composer->getInstallationManager()->getInstallPath($package);
-        $jsonFile = new \Composer\Json\JsonFile($packagePath . DIRECTORY_SEPARATOR . 'composer.json', new \Composer\Util\RemoteFilesystem($this->io));
+        $jsonFile = new JsonFile($packagePath . DIRECTORY_SEPARATOR . 'composer.json', new RemoteFilesystem($this->io));
         $packageJson = $jsonFile->read();
         $packageVendorDir = !empty($packageJson['config']['vendor-dir']) ? $this->filesystem->normalizePath($packageJson['config']['vendor-dir']) : $defaultVendorDir;
 
